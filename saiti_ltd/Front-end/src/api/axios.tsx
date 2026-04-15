4f3684d715686adce21a3ba60476ec1abdaf06e1
Kiariengigi <64465044+Kiariengigi@users.noreply.github.com>
2026-04-15 13:26:03 +0300

    fix

--- saiti_ltd/Front-end/src/api/axios.tsx
+++ saiti_ltd/Front-end/src/api/axios.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const apiBaseUrl = import.meta.env.VITE_API_BASE_URL ?? 'https://saiti-ltd-backend.onrender.com'
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL ?? 'https://saiti-ltd-backend.onrender.com/api'
 
 export default axios.create({
     baseURL: apiBaseUrl,
