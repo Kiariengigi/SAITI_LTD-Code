8c226dd0b18e7a4499b6559dd8a230226f72c7ff
Kiariengigi <64465044+Kiariengigi@users.noreply.github.com>
2026-04-15 13:12:54 +0300

    fix

--- saiti_ltd/Front-end/src/api/axios.tsx
+++ saiti_ltd/Front-end/src/api/axios.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const apiBaseUrl = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:7000/api/'
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL 
 console.log(apiBaseUrl)
 
 export default axios.create({
